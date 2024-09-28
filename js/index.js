16c96f73333629e5228f08aeb799cbeb8b495600
Nhan Hiep <nhan.hiep.11@gmail.com>
2024-09-28 18:57:34 +0700

    Oke nho

--- js/index.js
+++ js/index.js
@@ -94,3 +94,24 @@ window.onload = function() {
 // window.onload = function() {
 //     shipButtons[3].style.backgroundColor = '#ccc';
 // };
+document.addEventListener('DOMContentLoaded', function() {
+    const stationModal = document.getElementById('stationModal');
+    const overlay = document.getElementById('overlay');
+
+    stationModal.addEventListener('show.bs.modal', function (event) {
+        overlay.style.display = 'block'; // Hiện lớp overlay khi modal mở
+        const button = event.relatedTarget; // Thẻ a đã được nhấp
+        const title = button.getAttribute('data-bs-title'); // Lấy tiêu đề từ thuộc tính data-bs-title
+        const content = button.getAttribute('data-bs-content'); // Lấy nội dung từ thuộc tính data-bs-content
+
+        const modalTitle = stationModal.querySelector('.modal-title');
+        const modalContent = stationModal.querySelector('#stationContent');
+
+        modalTitle.textContent = title; // Đặt tiêu đề cho modal
+        modalContent.textContent = content; // Đặt nội dung cho modal
+    });
+
+    stationModal.addEventListener('hidden.bs.modal', function () {
+        overlay.style.display = 'none'; // Ẩn lớp overlay khi modal đóng
+    });
+});
