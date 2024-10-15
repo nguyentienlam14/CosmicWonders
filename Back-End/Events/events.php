aa82fc56b2c9fabbdc237d249cf4b3ec88edfaa0
hung <hugngdz@gmail.com>
2024-10-15 16:22:28 +0700

    final

--- Back-End/Events/events.php
+++ Back-End/Events/events.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-
-</head>
-
-<body>
 
     <?php
     $servername = "localhost";
@@ -61,11 +49,3 @@
     ?>
 
     
-
-
-    <script>
-        
-    </script>
-</body>
-
-</html>
